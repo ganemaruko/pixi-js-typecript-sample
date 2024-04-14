b4c68f4780be3d4ae6ca9b09371841c128620c56
ganemaruko <githubwork1@gmail.com>
2024-04-14 13:52:27 +0900

    移動エフェクトの実装

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # action-game
+
+Action gameを作りたいぜ
+
+## やりたいアイデア
---
+++ src/spineBoy.ts
@@ -0,0 +1,24 @@
+import { Spine } from "@pixi/spine-pixi";
+import { Container } from "pixi.js";
+
+// Class for handling the character Spine and its animations.
+export class SpineBoy
+{
+    public view: Container;
+    public spine: Spine;
+    constructor()
+    {
+        // Create the main view.
+        this.view = new Container();
+
+        /** -- INSERT CODE HERE -- */
+                // Create the Spine instance using the preloaded Spine asset aliases.
+                this.spine = Spine.from({
+                    skeleton: 'spineSkeleton',
+                    atlas: 'spineAtlas',
+                });
+        
+                // Add the spine to the main view.
+                this.view.addChild(this.spine);
+    }
+}
